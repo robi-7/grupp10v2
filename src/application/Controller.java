d7dc68e2e9b92d32207cacba428cc59090657724
OgnjenRadic <56583990+OgnjenRadic@users.noreply.github.com>
2020-09-24 16:59:59 +0200

    Student register, find and delete

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -13,6 +13,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 
 import javafx.scene.*;
 import javafx.fxml.FXML;
@@ -42,132 +43,132 @@ import database.DataAccessLayer;
 public class Controller implements Initializable {
 	//VARIABLES
 	private DataAccessLayer database;
-	
-	
+
+
 	//TAB: STUDENT
-    @FXML
-    private Tab tabStudent;
-    @FXML
-    private TextField txtStudentID;
-    @FXML
-    private TextField txtStudentName;
-    @FXML
-    private TextField txtStudentSsn;
-    @FXML
-    private TextField txtStudentAddress;
-    @FXML
-    private TextField txtStudentEmail;
-    @FXML
-    private Button btnStudentRegister;
-    @FXML
-    private Button btnStudentDelete;
-    @FXML
-    private Button btnStudentFind;
-    @FXML
-    private Label lblStudentStatus;
-    //TABLE: STUDENTS
-    @FXML
-    private TableView<Student> tblStudents;
-    @FXML
-    private TableColumn<Student, String> tblColStudentStudentID;
-    @FXML
-    private TableColumn<Student, String> tblColStudentName;
-    @FXML
-    private TableColumn<Student, String> tblColStudentSsn;
-    @FXML
-    private TableColumn<Student, String> tblColStudentAddress;
-    @FXML
-    private TableColumn<Student, String> tblColStudentEmail;
-    //TABLE: COURSES
-    @FXML
-    private TableView<StudentCourses> tblStudentCourses;
-    @FXML
-    private TableColumn<StudentCourses, String> tblColStudentCourseCourseID;
-    @FXML
-    private TableColumn<StudentCourses, String> tblColStudentCourseName;
-    @FXML
-    private TableColumn<StudentCourses, String> tblColStudentCourseCredits;
-    @FXML
-    private TableColumn<StudentCourses, String> tblColStudentCourseGrade;
-    
-    //TAB: COURSE
-    @FXML    
-    private Tab tabCourse;
-    @FXML
-    private TextField txtCourseID;
-    @FXML
-    private TextField txtCourseName;
-    @FXML
-    private TextField txtCourseCredits;
-    @FXML
-    private Button btnCourseRegister;
-    @FXML
-    private Button btnCourseDelete;
-    @FXML
-    private Button btnCourseFind;
-    @FXML
-    private Label lblCourseStatus;
-    @FXML
-    private TableView<?> tblCourses;
-    @FXML
-    private TableView<?> tblCourseStudents;
-    
-    //TAB: REGISTER
-    @FXML    
-    private Tab tabRegister;
-    @FXML
-    private Label lblRegisterStatus;
-    @FXML
-    private TableView<?> tblRegisterCourses;
-    @FXML
-    private TableView<?> tblRegisterStudents;
-    @FXML
-    private Button btnRegisterStudies;
-    @FXML
-    private Button btnRegisterHasStudied;
-    @FXML
-    private ComboBox<?> cbxGrade;
-	
-    //INITIALIZATOR
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-    	//Tab: Student, Table: Student
-    	tblColStudentStudentID.setCellValueFactory(new PropertyValueFactory<Student,String>("studentID"));
-    	tblColStudentName.setCellValueFactory(new PropertyValueFactory<Student,String>("name"));
-    	tblColStudentSsn.setCellValueFactory(new PropertyValueFactory<Student,String>("ssn"));
-    	tblColStudentAddress.setCellValueFactory(new PropertyValueFactory<Student,String>("address"));
-    	tblColStudentEmail.setCellValueFactory(new PropertyValueFactory<Student,String>("email"));
-    	
-    	//Tab: Student, Table: Course
-    	tblColStudentCourseCourseID.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("courseID"));
-    	tblColStudentCourseName.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("name"));
-    	tblColStudentCourseCredits.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("credits"));
-    	tblColStudentCourseGrade.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("grade"));
-    	
-    	try {
+	@FXML
+	private Tab tabStudent;
+	@FXML
+	private TextField txtStudentID;
+	@FXML
+	private TextField txtStudentName;
+	@FXML
+	private TextField txtStudentSsn;
+	@FXML
+	private TextField txtStudentAddress;
+	@FXML
+	private TextField txtStudentEmail;
+	@FXML
+	private Button btnStudentRegister;
+	@FXML
+	private Button btnStudentDelete;
+	@FXML
+	private Button btnStudentFind;
+	@FXML
+	private Label lblStudentStatus;
+	//TABLE: STUDENTS
+	@FXML
+	private TableView<Student> tblStudents;
+	@FXML
+	private TableColumn<Student, String> tblColStudentStudentID;
+	@FXML
+	private TableColumn<Student, String> tblColStudentName;
+	@FXML
+	private TableColumn<Student, String> tblColStudentSsn;
+	@FXML
+	private TableColumn<Student, String> tblColStudentAddress;
+	@FXML
+	private TableColumn<Student, String> tblColStudentEmail;
+	//TABLE: COURSES
+	@FXML
+	private TableView<StudentCourses> tblStudentCourses;
+	@FXML
+	private TableColumn<StudentCourses, String> tblColStudentCourseCourseID;
+	@FXML
+	private TableColumn<StudentCourses, String> tblColStudentCourseName;
+	@FXML
+	private TableColumn<StudentCourses, String> tblColStudentCourseCredits;
+	@FXML
+	private TableColumn<StudentCourses, String> tblColStudentCourseGrade;
+
+	//TAB: COURSE
+	@FXML    
+	private Tab tabCourse;
+	@FXML
+	private TextField txtCourseID;
+	@FXML
+	private TextField txtCourseName;
+	@FXML
+	private TextField txtCourseCredits;
+	@FXML
+	private Button btnCourseRegister;
+	@FXML
+	private Button btnCourseDelete;
+	@FXML
+	private Button btnCourseFind;
+	@FXML
+	private Label lblCourseStatus;
+	@FXML
+	private TableView<?> tblCourses;
+	@FXML
+	private TableView<?> tblCourseStudents;
+
+	//TAB: REGISTER
+	@FXML    
+	private Tab tabRegister;
+	@FXML
+	private Label lblRegisterStatus;
+	@FXML
+	private TableView<?> tblRegisterCourses;
+	@FXML
+	private TableView<?> tblRegisterStudents;
+	@FXML
+	private Button btnRegisterStudies;
+	@FXML
+	private Button btnRegisterHasStudied;
+	@FXML
+	private ComboBox<?> cbxGrade;
+
+	//INITIALIZATOR
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		//Tab: Student, Table: Student
+		tblColStudentStudentID.setCellValueFactory(new PropertyValueFactory<Student,String>("studentID"));
+		tblColStudentName.setCellValueFactory(new PropertyValueFactory<Student,String>("name"));
+		tblColStudentSsn.setCellValueFactory(new PropertyValueFactory<Student,String>("ssn"));
+		tblColStudentAddress.setCellValueFactory(new PropertyValueFactory<Student,String>("address"));
+		tblColStudentEmail.setCellValueFactory(new PropertyValueFactory<Student,String>("email"));
+
+		//Tab: Student, Table: Course
+		tblColStudentCourseCourseID.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("courseID"));
+		tblColStudentCourseName.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("name"));
+		tblColStudentCourseCredits.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("credits"));
+		tblColStudentCourseGrade.setCellValueFactory(new PropertyValueFactory<StudentCourses,String>("grade"));
+
+		try {
 			this.viewStudents();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-    }
-    
-    
-    //CONSTRUCTOR
+	}
+
+
+	//CONSTRUCTOR
 	public Controller() {
 		try 
 		{
-		database = new DataAccessLayer();
+			database = new DataAccessLayer();
 		} 
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+
 	//METHODS
 	public void viewStudents() throws SQLException {
 		ResultSet rs = database.viewStudents();
-		
+
 		ObservableList<Student> data = FXCollections.observableArrayList();
 		while(rs.next()) {
 			String studentID = rs.getString(1);
@@ -179,46 +180,97 @@ public class Controller implements Initializable {
 		}
 		tblStudents.setItems(data);
 	}
-	
-	public ResultSet getStudent(String studentID) throws SQLException {
-		return database.getStudent(studentID);
-	}
-	@FXML
-	public void registerStudent() throws SQLException {
-		lblStudentStatus.setText("");
-	try {
-		String name = txtStudentName.getText();
-		String ssn = txtStudentSsn.getText();
-		String address = txtStudentAddress.getText();
-		String email = txtStudentEmail.getText();
-		
-		if (ssn.length() == 10) {
+    @FXML
+	public void findStudent()  {
+    	lblStudentStatus.setText("");
+    	try {
+			String studentID = txtStudentID.getText().toUpperCase();
+			ResultSet rs = database.getStudent(studentID);
+			rs.next();
+			txtStudentName.setText(rs.getString(2));		
+			txtStudentSsn.setText(rs.getString(3));
+			txtStudentAddress.setText(rs.getString(4));
+			txtStudentEmail.setText(rs.getString(5));
+			for (Student student : tblStudents.getItems()) {
+				if (studentID.equals(student.getStudentID())) {
+					tblStudents.getSelectionModel().select(student);
+				}
+			} 
 			
-			String newStudentID = database.registerStudent(name,  ssn,  address, email);
-			this.viewStudents();
+			tblStudentSelected();
+		} catch(SQLException e) {
+			lblStudentStatus.setText("Student ID does not exist");
+			txtStudentName.setText("");		
+			txtStudentSsn.setText("");
+			txtStudentAddress.setText("");
+			txtStudentEmail.setText("");
 			tblStudentCourses.setItems(null);
-			tblStudents.getSelectionModel().selectLast();
-			int lastIndex = tblStudents.getSelectionModel().getSelectedIndex();
-			tblStudents.scrollTo(lastIndex); 
-			txtStudentID.setText(newStudentID);
-		} else { 
-			lblStudentStatus.setText("Social security number must consist of 10 numbers");
+			tblStudents.getSelectionModel().clearSelection();
+		} 
+		
+	} 
+	@FXML
+	public void registerStudent()  {
+		lblStudentStatus.setText("");
+		try {
+			String name = txtStudentName.getText();
+			String ssn = txtStudentSsn.getText();
+			String address = txtStudentAddress.getText();
+			String email = txtStudentEmail.getText();   
+
+			if (ssn.length() == 10) {
+				String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+(?:\\\\.[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\\\.[a-zA-Z0-9-]+)*$";
+				Pattern pattern = Pattern.compile(regex);
+				if(pattern.matcher(email).matches()){
+
+					String newStudentID = database.registerStudent(name,  ssn,  address, email);
+					this.viewStudents();
+					tblStudentCourses.setItems(null);
+					tblStudents.getSelectionModel().selectLast();
+					int lastIndex = tblStudents.getSelectionModel().getSelectedIndex();
+					tblStudents.scrollTo(lastIndex); 
+					txtStudentID.setText(newStudentID);
+				} else {
+					lblStudentStatus.setText("Please enter valid email");
+				}
+
+			} else { 
+				lblStudentStatus.setText("Social security number must consist of 10 numbers");
+			}
+		}
+		catch (SQLException e) {
+			lblStudentStatus.setText("A student with that social security number already exists");
 		}
 	}
-	catch (SQLException e) {
-		lblStudentStatus.setText("A student with that social security number already exists");
-	}
-	}
-	
-	public void deleteStudent(String studentID) throws SQLException {
-		database.deleteStudent(studentID);
-	}
+    @FXML
+	public void deleteStudent() throws SQLException {
+    	lblStudentStatus.setText("");
+    	try {
+			if (!tblStudents.getSelectionModel().isEmpty()) {
+				String studentID = tblStudents.getSelectionModel().getSelectedItems().get(0).getStudentID(); 
+				database.deleteStudent(studentID);
+				tblStudents.setItems(null);
+				tblStudentCourses.setItems(null);
+				viewStudents();
+				txtStudentID.setText("");	
+				txtStudentName.setText("");		
+				txtStudentSsn.setText("");
+				txtStudentAddress.setText("");
+				txtStudentEmail.setText("");
+			} else {
+				lblStudentStatus.setText("Please select a student in the list below first");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		}
+
 	@FXML
 	public void tblStudentSelected() throws SQLException {
 		if(!tblStudents.getSelectionModel().isEmpty()) {
 			int index = tblStudents.getSelectionModel().getSelectedIndex();
 			String studentID = tblColStudentStudentID.getCellData(index);
-			
+
 			ResultSet course = database.getStudentCourses(studentID);
 			ObservableList<StudentCourses> data = FXCollections.observableArrayList();
 			while(course.next()) {
@@ -229,7 +281,7 @@ public class Controller implements Initializable {
 				data.add(new StudentCourses(courseID,name,credits,grade));
 			}
 			tblStudentCourses.setItems(data);
-			ResultSet student = this.getStudent(studentID);
+			ResultSet student = database.getStudent(studentID);
 			student.next();
 			txtStudentID.setText(student.getString(1));
 			txtStudentName.setText(student.getString(2));
@@ -238,7 +290,7 @@ public class Controller implements Initializable {
 			txtStudentEmail.setText(student.getString(5));
 		}
 	}
-	
+
 	public ResultSet getCourse(String courseID) throws SQLException {
 		return database.getCourse(courseID);
 	}
@@ -248,24 +300,24 @@ public class Controller implements Initializable {
 	public void deleteCourse(String courseID) throws SQLException {
 		database.deleteCourse(courseID);
 	}
-	
+
 	public ResultSet getStudies(String courseID, String studentID) throws SQLException {
 		return database.getStudies(courseID, studentID);
 	}
-	
+
 	public void registerStudies(String courseID, String studentID) throws SQLException {
 		database.registerStudies(courseID, studentID);
 	}
 	public void deleteStudentFromCourse(String courseID, String studentID) throws SQLException {
 		database.deleteStudies(courseID, studentID);
 	}
-	
+
 	public ResultSet getHasStudied(String courseID, String studentID) throws SQLException {
 		return database.getHasStudied(courseID, studentID);
 	}
 	public void registerHasStudied(String grade, String courseID, String studentID) throws SQLException {
 		database.registerHasStudied(grade, courseID, studentID);
 	}
-	
-	
+
+
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -30,28 +30,8 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {		
 		Controller controller = new Controller();
-		try {
-			ResultSet student = controller.getStudent("S004");
-			ResultSet course = controller.getCourse("C005");
-			ResultSet studies = controller.getStudies("C001", "S002");
-			ResultSet hasStudied = controller.getHasStudied("C005","S004");
-			
-			while(student.next()) {
-				System.out.println(student.getString(1) + " - " + student.getString(2) + " - " + student.getString(3) + " - " + student.getString(4) + " - " + student.getString(5));
-			}
-			while(course.next()) {
-				System.out.println(course.getString(1) + " - " + course.getString(2) + " - " + course.getString(3));
-			}
-			while(studies.next()) {
-				System.out.println(studies.getString(1) + " - " + studies.getString(2));
-			}
-			while(hasStudied.next()) {
-				System.out.println(hasStudied.getString(1) + " - " + hasStudied.getString(2) + " - " + hasStudied.getString(3));
-			}
-//			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		
+		
 		launch(args);
-	}
+}
 }
